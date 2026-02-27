eb226fb121badab559f6ff2019aa103612d4ae87
Tiago <tiagohirute@gmail.com>
2026-02-27 01:41:32 -0300

    chore: backup commit

---
+++ src/Backend/MyRecipeBook.API/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyRecipeBook.API.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        public IActionResult Register()
+        {
+
+            return Created();
+
+        }
+
+    }
+}
